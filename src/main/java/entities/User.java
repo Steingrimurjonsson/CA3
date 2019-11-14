e2f611f8ebbc1cd599bb15a99cd78d9d9fdcb6d2
Stein <steingrimurkj@gmail.com>
2019-11-14 21:35:02 +0100

    Update User.java

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -55,7 +55,7 @@ public class User implements Serializable {
 
   public User(String userName, String userPass) {
     this.userName = userName;
-    this.userPass = BCrypt.hashpw(userPass, BCrypt.gensalt());
+    this.userPass = BCrypt.hashpw(userPass, BCrypt.gensalt(12));
   }
 
   public String getUserName() {
