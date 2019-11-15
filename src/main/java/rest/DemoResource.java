8153acec0ce84ec44229b89f27675cb179a0ba7a
Stein <steingrimurkj@gmail.com>
2019-11-15 10:03:32 +0100

    Update DemoResource.java

--- src/main/java/rest/DemoResource.java
+++ src/main/java/rest/DemoResource.java
@@ -68,35 +68,5 @@ public class DemoResource {
         String thisuser = securityContext.getUserPrincipal().getName();
         return "{\"msg\": \"Hello to (admin) User: " + thisuser + "\"}";
     }
-    @GET
-    @Path("/populate")
-    @Produces(MediaType.APPLICATION_JSON)
-    public String populate() {
-        EntityManager em = EMF.createEntityManager();
-        try {
-         User user = new User("user", "1234");
-    User admin = new User("admin", "1234");
-    User both = new User("user_admin", "1234");
-   
-    em.getTransaction().begin();
-    Role userRole = new Role("user");
-    Role adminRole = new Role("admin");
-    user.addRole(userRole);
-    admin.addRole(adminRole);
-    both.addRole(userRole);
-    both.addRole(adminRole);
-    em.persist(userRole);
-    em.persist(adminRole);
-    em.persist(user);
-    em.persist(admin);
-    em.persist(both);
-    em.getTransaction().commit();
-    System.out.println("PW: " + user.getUserPass());
-    System.out.println("Testing user with OK password: " + user.verifyPassword("1234"));
-    System.out.println("Testing user with wrong password: " + user.verifyPassword("12345"));
-        } finally {
-            em.close();
-        }
-        return "worked";
-    }
+
 }
